7373e69c3a60abc9d77166d8ac56cc3f5b3f3522
Seung-hyun Kim <seunghyun4525@gmail.com>
2022-11-04 06:58:55 +0900

    feat: 🎸 OAuth 관련 상수 파일 생성

---
+++ src/constants/oauth.js
@@ -0,0 +1,8 @@
+// 참고: 이미 삭제된 Github OAuth 정보들입니다.
+export const CLIENT_ID = "8401b2ab1be3f5f33623";
+export const CLIENT_SECRETS = "1eec25b6176d61a43bbb083b3e6770a45cfb3d39";
+export const CALLBACK_URL = "http://localhost:3000/oauth/callback";
+
+export const GITHUB_AUTH_CODE_SERVER = "/login/oauth/authorize";
+export const GITHUB_AUTH_TOKEN_SERVER = "/login/oauth/access_token";
+export const GITHUB_API_SERVER = "/user";
